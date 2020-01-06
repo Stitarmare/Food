600f9f9cf106c6771f3e5b1c570f620327ac620a
Swapnil Gaikwad <swapnil.gaikwad@neosofttech.com>
2020-01-06 18:37:57 +0530

    Profile Screen Changes

--- lib/ProfilePage/ProfileScreen.dart
+++ lib/ProfilePage/ProfileScreen.dart
@@ -13,35 +13,75 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // appBar: AppBar(
-      //   title: Text('My Profile'),
-      //   backgroundColor: Colors.transparent,
-      // ),
-      body: SingleChildScrollView(child: _getMainView()),
+    return SafeArea(
+          child: Scaffold(
+        appBar: PreferredSize(
+            preferredSize: Size(MediaQuery.of(context).size.width, MediaQuery.of(context).size.height * 0.2),
+            child: Stack(
+              overflow: Overflow.visible,
+              children: <Widget>[
+                Center(child: Image.asset('assets/BlurImage/Group1612.png',fit: BoxFit.fill,width:MediaQuery.of(context).size.width,)),
+                Container(
+                  //color: Colors.blueAccent,
+                  child: Container(
+                    margin: EdgeInsets.fromLTRB(0, 34, 0, 0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(width: 25,),
+                        Text(
+                          "My Profile",
+                          style: TextStyle(fontSize: 20, color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: MediaQuery.of(context).size.width / 2.5,
+                  top: MediaQuery.of(context).size.height / 6.8,
+                  child: ClipOval(
+                    //radius: 50,
+                    // child: Card(
+                    //   elevation: 10,
+                      child: Image.asset('assets/ProfileImage/MaskGroup15.png',fit: BoxFit.cover,width: 82.5,height: 82.5,),
+                    // ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          body: Center(),
+        // appBar: AppBar(
+        //   title: Text('My Profile'),
+        //   backgroundColor: Colors.transparent,
+        // ),
+        //body: SingleChildScrollView(child: _getMainView()),
+      ),
     );
   }
 }
 
-Widget _getMainView() {
-  return Container(
-      child: Stack(
-    children: <Widget>[
-      Image.asset('assets/BlurImage/Group1612.png'),
-      Positioned(
-        child: Text(
-          'My Profile',
-          style: TextStyle(color: Colors.white),
-        ),
-        left: 12,
-        top: 10,
-      ),
-      Positioned(
-        child: ClipOval(
-          child: Image.asset('assets/ProfileImage/MaskGroup15.png'),
-        ),
-        left: 20,
-      ),
-    ],
-  ));
-}
+// Widget _getMainView() {
+//   return Container(
+//       child: Stack(
+//     children: <Widget>[
+//       Image.asset('assets/BlurImage/Group1612.png'),
+//       Positioned(
+//         child: Text(
+//           'My Profile',
+//           style: TextStyle(color: Colors.white),
+//         ),
+//         left: 12,
+//         top: 10,
+//       ),
+//       Positioned(
+//         child: ClipOval(
+//           child: Image.asset('assets/ProfileImage/MaskGroup15.png'),
+//         ),
+//         left: 20,
+//       ),
+//     ],
+//   ));
+// }
