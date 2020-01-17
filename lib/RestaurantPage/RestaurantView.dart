e0020f1f26f12436582073b4182ba5ff263a4df4
Swapnil Gaikwad <swapnil.gaikwad@neosofttech.com>
2020-01-17 17:45:56 +0530

    RestaurantView Menu Button changes

--- lib/RestaurantPage/RestaurantView.dart
+++ lib/RestaurantPage/RestaurantView.dart
@@ -1,17 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:foodzi/Utils/String.dart';
-<<<<<<< HEAD
+
 import 'package:foodzi/widgets/MenuItemDropDown.dart';
-=======
+
 import 'package:foodzi/theme/colors.dart';
 
-<<<<<<< HEAD
 import 'package:foodzi/BottomTabbar/BottomTabbarRestaurant.dart';
 
-=======
->>>>>>> a99697964e067772ead01e88d63c8117f12c8ee2
->>>>>>> eb05b5c51a71c0b05d92ea85326877e200e9ab9f
 class RestaurantView extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -22,7 +18,7 @@ class RestaurantView extends StatefulWidget {
 class _RestaurantViewState extends State<RestaurantView> {
   final GlobalKey _menuKey = new GlobalKey();
   bool _switchvalue = false;
- 
+  bool isselected=false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -193,17 +189,39 @@ class _RestaurantViewState extends State<RestaurantView> {
             ),
             SizedBox(
               child: OutlineButton(
+                 
                   child: Text(
-                    "Menu",
+                    "Menu", 
                     style: TextStyle(
                         fontSize: 12,
                         fontFamily: 'gotham',
                         fontWeight: FontWeight.w500,
-                        color: greytheme100),
+                        // color: (isselected) ? redtheme :greytheme100
+                        color: redtheme
+                        ),
                   ),
-                  onPressed: null,
-                  borderSide: BorderSide(color: greentheme100),
+                  //borderSide: (isselected) ? BorderSide(color:redtheme):BorderSide(color: greytheme100),
+                  borderSide: BorderSide(color:redtheme),
+                  onPressed: () {
+                    setState(() {
+                     if(isselected == false){
+                       isselected = true;
+                       
+                     }
+                     else{
+                       isselected = false;
+                     }
+
+                    });
+                    showDialog(
+                        context: context,
+                        builder: (_) => MenuItem(),
+                        barrierDismissible: true);
+                  },
+                  
+                  
                   shape: new RoundedRectangleBorder(
+                    
                     borderRadius: new BorderRadius.circular(12.0),
                   )),
               height: 22,
@@ -214,51 +232,9 @@ class _RestaurantViewState extends State<RestaurantView> {
       ),
     );
   }
-  
- 
-// void _select(Item item){
-//   setState(() {
-    
-//   });
-// }
 
-<<<<<<< HEAD
-  Widget _getOptionsformenu() {
-    return Row(
-      children: <Widget>[
-        SizedBox(width: 20),
-        Text('veg only'),
-        Switch(
-          activeColor: Colors.red,
-          onChanged: (bool value) {
-            setState(() {
-              this._switchvalue = value;
-            });
-          },
-          value: this._switchvalue,
-        ),
-        SizedBox(
-          width:180,
-        ),
-        SizedBox(
-          child: new OutlineButton(
-              child: Text("Menu"),
-              onPressed: (){
-                // dynamic state = _menuKey.currentState;
-                // state.showButtonMenu();
-                showDialog(
-                  context: context,
-                  builder: (_)=> MenuItem(),
-                  barrierDismissible: true
-                );
-              },
-              shape: new RoundedRectangleBorder(
-                  borderRadius: new BorderRadius.circular(12.0))),
-          height: 30,
-          width: 80,
-        )
-      ],
-=======
+
+
   Widget _menuItemList() {
     return SliverGrid(
       gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
@@ -383,186 +359,16 @@ class _RestaurantViewState extends State<RestaurantView> {
           ),
         );
       }, childCount: 7),
->>>>>>> a99697964e067772ead01e88d63c8117f12c8ee2
     );
   }
 
-//   Widget _dropdownMenuItem(){
-//     return Container(
-//       decoration: BoxDecoration(borderRadius: BorderRadius.circular(12)),
-//       child: DropdownButton<Item>(
-//                onChanged: (Item value) {
-//                 setState(() {
-//                   selectedMenu = value; 
-//                 });
-//               },
-//               value: selectedMenu,
-//               isExpanded: false,
-//               items: _menu
-//                   .map((Item menu){
-//                     return DropdownMenuItem(
-//                       child: ListTile(leading: Text(menu.itemName),trailing: Text(menu.itemCount),),
-//                       value: menu,
-//                     );
-//                   }).toList()
-//     ));
 
-//   }
-// }
 
 }
 
-class Item{
+class Item {
   String itemName;
   String itemCount;
-  Item({this.itemName,this.itemCount});
+  Item({this.itemName, this.itemCount});
 }
 
-// import 'package:flutter/material.dart';
-// import 'package:foodzi/Utils/String.dart';
-
-// class RestaurantView extends StatefulWidget {
-//   @override
-//   State<StatefulWidget> createState() {
-//     return _RestaurantViewState();
-//   }
-// }
-
-// class _RestaurantViewState extends State<RestaurantView> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         backgroundColor: Colors.transparent,
-//         elevation: 0,
-//         actions: <Widget>[
-//           IconButton(
-//             icon: Icon(Icons.perm_identity),
-//             onPressed: () {},
-//           )
-//         ],
-//       ),
-//       body: SingleChildScrollView(
-//         child: _buildMainView(),
-//       ),
-//     );
-//   }
-
-//   Widget _buildMainView() {
-//     return Container(
-//       child: Column(
-//         children: <Widget>[
-//           _getmainviewTableno(),
-//           SizedBox(
-//             height: 40,
-//           ),
-//           _getOptionscollections()
-//         ],
-//       ),
-//     );
-//   }
-
-//   Widget _getmainviewTableno() {
-//     return Container(
-//       margin: EdgeInsets.fromLTRB(15, 0, 15, 0),
-//       child: Card(
-//         child: Column(
-//           crossAxisAlignment: CrossAxisAlignment.start,
-//           children: <Widget>[
-//             Row(
-//               children: <Widget>[
-//                 SizedBox(
-//                   width: 40,
-//                 ),
-//                 Text(
-//                   'That’s Amore',
-//                   style: TextStyle(fontSize: 30),
-//                 ),
-//               ],
-//             ),
-//             Divider(
-//               thickness: 2,
-//               //endIndent: 10,
-//               //indent: 10,
-//             ),
-//             Row(
-//               children: <Widget>[
-//                 SizedBox(
-//                   width: 26,
-//                 ),
-//                 Image.asset('assets/DineInImage/Group1504.png'),
-//                 SizedBox(
-//                   width: 20,
-//                 ),
-//                 Text('Dine-in')
-//               ],
-//             ),
-//             Row(
-//               children: <Widget>[SizedBox(width: 76), Text('Add Table Number')],
-//             )
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-
-//   Widget _getOptionscollections() {
-//     return Container(
-//       margin: EdgeInsets.fromLTRB(12, 0, 12, 0),
-//       child: Column(
-//         children: <Widget>[
-//           Row(
-//             children: <Widget>[
-//               SizedBox(
-//                 height: 22,
-//                 // width: 100,
-//                 child: new OutlineButton(
-//                     child: new Text("Breakfast"),
-//                     onPressed: null,
-//                     shape: new RoundedRectangleBorder(
-//                         borderRadius: new BorderRadius.circular(2.0))),
-//               ),
-//               SizedBox(
-//                 width: 10,
-//               ),
-//               SizedBox(
-//                 height: 22,
-//                 //   width: 80,
-//                 child: new OutlineButton(
-//                     child: new Text("Lunch"),
-//                     onPressed: null,
-//                     shape: new RoundedRectangleBorder(
-//                         borderRadius: new BorderRadius.circular(2.0))),
-//               ),
-//               SizedBox(
-//                 width: 10,
-//               ),
-//               SizedBox(
-//                 height: 22,
-//                 //  width: 70,
-//                 child: new OutlineButton(
-//                     child: new Text("Dinner"),
-//                     onPressed: null,
-//                     shape: new RoundedRectangleBorder(
-//                         borderRadius: new BorderRadius.circular(2.0))),
-//               ),
-//             ],
-//           ),
-//           Column(
-//             children: <Widget>[
-//               SizedBox(height: 10),
-//               SizedBox(
-//                 height: 22,
-//                 child: new OutlineButton(
-//                     child: new Text("Deserts"),
-//                     onPressed: null,
-//                     shape: new RoundedRectangleBorder(
-//                         borderRadius: new BorderRadius.circular(2.0))),
-//               ),
-//             ],
-//           )
-//         ],
-//       ),
-//     );
-//   }
-// }
