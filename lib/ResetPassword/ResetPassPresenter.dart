ca1b7cfe9a9e5e5c98b288f3872240539e7b432c
Swapnil Gaikwad <swapnil.gaikwad@neosofttech.com>
2020-01-21 17:01:22 +0530

    ResetPasswordPresenter changes

--- lib/ResetPassword/ResetPassPresenter.dart
+++ lib/ResetPassword/ResetPassPresenter.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:foodzi/network/ApiBaseHelper.dart';
+import 'package:foodzi/network/api_model.dart';
 
 import 'package:foodzi/network/url_constant.dart';
 //import 'package:foodzi/Utils/globle.dart';
@@ -24,6 +25,19 @@ class ResetpasswordPresenter extends ResetpassContractor {
       'password_confirmation': EncryptionAES.getData(password)
     }).then((value) {
       print(value);
+      switch (value.result) {
+        case SuccessType.success:
+          print("success");
+          //print(value.model);
+          //Globle().loginModel = value.model;
+          //Globle().authKey = value.model.token;
+           mregisterView.resetpasssuccess();
+          break;
+        case SuccessType.failed:
+          print("failed");
+         mregisterView.resetpassfailed();
+          break;
+      }
       // if (value['status_code'] == 200) {
       //   mregisterView.resetpasssuccess();
       // } else {
