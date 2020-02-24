3105c5455fa7678b2feb79380adc45707dfb7191
Swapnil Gaikwad <swapnil.gaikwad@neosofttech.com>
2020-02-24 12:31:16 +0530

    Placeholder image added for profile image

--- lib/AddItemPage/AddItemPageView.dart
+++ lib/AddItemPage/AddItemPageView.dart
@@ -604,7 +604,7 @@ class _AddItemPageViewState extends State<AddItemPageView>
                           height: 36,
                           child: ToggleButtons(
                               borderColor: greytheme1300,
-                              fillColor: redtheme,
+                              fillColor: getColorByHex(Globle().colorscode),
                               borderWidth: 2,
                               selectedBorderColor: Colors.transparent,
                               selectedColor: Colors.white,
--- lib/MenuDropdownCategory/MenuItemDropDown.dart
+++ lib/MenuDropdownCategory/MenuItemDropDown.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:foodzi/MenuDropdownCategory/MenuItemDropDownContractor.dart';
 import 'package:foodzi/MenuDropdownCategory/MenuItemDropDownPresenter.dart';
 import 'package:foodzi/Models/CategoryListModel.dart';
+import 'package:foodzi/Utils/globle.dart';
+import 'package:foodzi/theme/colors.dart';
 
 class MenuItem extends StatefulWidget {
   var restaurantId;
@@ -78,7 +80,7 @@ class MenuItemState extends State<MenuItem>
                       _categorydata[index].name ?? "",
                       style: TextStyle(
                         color: _selectedMenu != null && _selectedMenu == index
-                            ? Color.fromRGBO(237, 29, 37, 1)
+                            ? getColorByHex(Globle().colorscode)
                             : Color.fromRGBO(118, 118, 118, 1),
                       ),
                     ),
