406216b3415d9f77a131146800b9495dd96477a1
webwerks1 <webwerks1@webwerkss-MacBook-Pro-4.local>
2020-03-12 13:22:51 +0530

    restaurant page changes

--- lib/RestaurantPage/RestaurantView.dart
+++ lib/RestaurantPage/RestaurantView.dart
@@ -27,7 +27,7 @@ import 'package:foodzi/widgets/imagewithloader.dart';
 class RestaurantView extends StatefulWidget {
   String title;
   int rest_Id;
-  
+
   int categoryid;
   RestaurantView({this.title, this.rest_Id, this.categoryid});
   @override
@@ -300,7 +300,9 @@ class _RestaurantViewState extends State<RestaurantView>
                         fontSize: 20,
                         fontFamily: 'gotham',
                         fontWeight: FontWeight.w600,
-                        color: ((Globle().colorscode) != null)?getColorByHex(Globle().colorscode):orangetheme),
+                        color: ((Globle().colorscode) != null)
+                            ? getColorByHex(Globle().colorscode)
+                            : orangetheme),
                   )
                 ],
               ),
@@ -355,7 +357,9 @@ class _RestaurantViewState extends State<RestaurantView>
             Transform.scale(
               scale: 0.7,
               child: CupertinoSwitch(
-                activeColor: ((Globle().colorscode) != null)?getColorByHex(Globle().colorscode):orangetheme,
+                activeColor: ((Globle().colorscode) != null)
+                    ? getColorByHex(Globle().colorscode)
+                    : orangetheme,
                 onChanged: (bool value) {
                   //  DialogsIndicator.showLoadingDialog(
                   //      context, _keyLoader, "Loading");
@@ -398,11 +402,16 @@ class _RestaurantViewState extends State<RestaurantView>
                         fontFamily: 'gotham',
                         fontWeight: FontWeight.w500,
                         color: (isselected)
-                            ? ((Globle().colorscode) != null)?getColorByHex(Globle().colorscode):orangetheme
+                            ? ((Globle().colorscode) != null)
+                                ? getColorByHex(Globle().colorscode)
+                                : orangetheme
                             : greytheme100),
                   ),
                   borderSide: (isselected)
-                      ? BorderSide(color: ((Globle().colorscode) != null)?getColorByHex(Globle().colorscode):orangetheme)
+                      ? BorderSide(
+                          color: ((Globle().colorscode) != null)
+                              ? getColorByHex(Globle().colorscode)
+                              : orangetheme)
                       : BorderSide(color: greytheme100),
                   //borderSide: BorderSide(color:redtheme),
                   onPressed: () async {
@@ -659,8 +668,9 @@ class _RestaurantViewState extends State<RestaurantView>
                                         fontSize: 14,
                                         fontStyle: FontStyle.normal,
                                         fontWeight: FontWeight.w600,
-                                        color:
-                                            ((Globle().colorscode) != null)?getColorByHex(Globle().colorscode):orangetheme),
+                                        color: ((Globle().colorscode) != null)
+                                            ? getColorByHex(Globle().colorscode)
+                                            : orangetheme),
                                   ),
                                 ),
                               ),
@@ -693,8 +703,9 @@ class _RestaurantViewState extends State<RestaurantView>
                                   },
                                   child: Container(
                                     decoration: BoxDecoration(
-                                        color:
-                                            ((Globle().colorscode) != null)?getColorByHex(Globle().colorscode):orangetheme,
+                                        color: ((Globle().colorscode) != null)
+                                            ? getColorByHex(Globle().colorscode)
+                                            : orangetheme,
                                         borderRadius: BorderRadius.only(
                                           bottomRight: Radius.circular(12.0),
                                         )),
