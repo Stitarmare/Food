5227d5371198ee450688ee4e80a94d5d6bd8dfbe
Pramod Singh <pramod.singh@neosofttech.com>
2020-06-12 10:57:13 +0530

    resolved bugs

--- lib/FAQ/faq_view.dart
+++ lib/FAQ/faq_view.dart
@@ -5,11 +5,11 @@ import 'package:flutter/services.dart';
 import 'package:foodzi/FAQ/faq_model.dart';
 import 'package:foodzi/FAQ/faq_que_ans.dart';
 import 'package:foodzi/Utils/String.dart';
+import 'package:foodzi/theme/colors.dart';
 
 class FAQVIew extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
     return _FAQVIewState();
   }
 }
@@ -20,9 +20,10 @@ Future<String> loadAsset() async {
 
 class _FAQVIewState extends State<FAQVIew> {
   List<FaqModelDatum> faqModelTitles = [];
+  int _selectedMenu;
+
   @override
   void initState() {
-    // TODO: implement initState
     loadJson();
     super.initState();
   }
@@ -35,47 +36,92 @@ class _FAQVIewState extends State<FAQVIew> {
     });
   }
 
+  _onSelected(index) {
+    setState(() {
+      _selectedMenu = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
       appBar: AppBar(
         title: Text("FAQS"),
       ),
       body: Container(
         child: SingleChildScrollView(
-          child: Column(
-            children: faqModelTitles
-                .map((e) => GestureDetector(
-                      onTap: () {
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) =>
-                                FaqQueAndView(e.data, e.title)));
-                      },
-                      child: Card(
-                        elevation: 3.0,
-                        child: Padding(
-                          padding: EdgeInsets.all(15),
-                          child: Row(
-                            children: <Widget>[
-                              Text(
-                                e.title,
-                                style: TextStyle(
-                                    color: Colors.black,
-                                    fontFamily: KEY_FONTFAMILY,
-                                    fontWeight: FontWeight.w600,
-                                    fontSize: FONTSIZE_15),
+            child:
+                // Column(
+                //   children: faqModelTitles
+                //       .map((e) => GestureDetector(
+                //             onTap: () {
+                //               Navigator.of(context).push(MaterialPageRoute(
+                //                   builder: (context) =>
+                //                       FaqQueAndView(e.data, e.title)));
+                //             },
+                //             child: Card(
+                //               elevation: 3.0,
+                //               child: Padding(
+                //                 padding: EdgeInsets.all(15),
+                //                 child: Row(
+                //                   children: <Widget>[
+                //                     Text(
+                //                       e.title,
+                //                       style: TextStyle(
+                //                           color: Colors.black,
+                //                           fontFamily: KEY_FONTFAMILY,
+                //                           fontWeight: FontWeight.w600,
+                //                           fontSize: FONTSIZE_15),
+                //                     ),
+                //                     Expanded(child: Container()),
+                //                     Text(">")
+                //                   ],
+                //                 ),
+                //               ),
+                //             ),
+                //           ))
+                //       .toList(),
+                // ),
+                Column(
+                    children: faqModelTitles
+                        .asMap()
+                        .map((index, e) => MapEntry(
+                            index,
+                            GestureDetector(
+                              onTap: () {
+                                _onSelected(index);
+
+                                Navigator.of(context).push(MaterialPageRoute(
+                                    builder: (context) =>
+                                        FaqQueAndView(e.data, e.title)));
+                              },
+                              child: Card(
+                                elevation: 3.0,
+                                color: _selectedMenu != null &&
+                                        _selectedMenu == index
+                                    ? greytheme100
+                                    : Colors.white,
+                                child: Padding(
+                                  padding: EdgeInsets.all(15),
+                                  child: Row(
+                                    children: <Widget>[
+                                      Text(
+                                        e.title,
+                                        style: TextStyle(
+                                            color: Colors.black,
+                                            fontFamily: KEY_FONTFAMILY,
+                                            fontWeight: FontWeight.w600,
+                                            fontSize: FONTSIZE_15),
+                                      ),
+                                      Expanded(child: Container()),
+                                      Text(">")
+                                    ],
+                                  ),
+                                ),
                               ),
-                              Expanded(child: Container()),
-                              Text(">")
-                            ],
-                          ),
-                        ),
-                      ),
-                    ))
-                .toList(),
-          ),
-        ),
+                            )))
+                        .values
+                        .toList())),
       ),
     );
   }
